ff4f3de6ade9b454532d63883aa3c668234a86c1
Vithurshan2002 <sangervithu2002@gmail.com>
2025-04-24 22:06:08 +0530

    saved

--- project/src/Pages/Home.jsx
+++ project/src/Pages/Home.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import './Home.css';
 import Question from '../Question.json';
 
@@ -7,31 +7,64 @@ const Home = () => {
     const [currentquestion, setcurrentquestion] = useState(0);
     const [score, setscore] = useState(0);
     const [scoreboard, setscoreboard] = useState(false);
+    const[timer,setTimer]=useState(10);
+
+    const AddData = (data) => {
+        if (data === Question[currentquestion].correctOption) {
+            setscore((pre) => pre + 1);
+        }
+    
+        if (currentquestion < Question.length - 1) {
+            setcurrentquestion((pre) => pre + 1);
+            setTimer(10);
+        } else {
+            setscoreboard(true);
+        }
+    }
+    
+
+    const Showdisplay = () => {
+        setcurrentquestion(0);
+        setscore(0);
+        setscoreboard(false);
+        setTimer(10);
+    }
+
+    useEffect(()=>{
+        let interval;
+        if(timer>0 && !scoreboard){
+            interval=setInterval(()=>{
+                setTimer((previous)=>previous-1)
+            },1000)
+        }
+        else{
+            clearInterval(interval);
+            setscoreboard(true);
+        }
+        return()=>clearInterval(interval);
+    },[timer,scoreboard]);
 
     return (
 
         <div className="maincontainer">
             {
                 scoreboard ? <div className="scoreboard"  >
-                    <p>Your Score:1/3</p>
-                    <button>Restart</button>
+                    <p>Your Score:{score}/{Question.length}</p>
+                    <button onClick={Showdisplay}>Restart</button>
                 </div> : <div className='container' /* style={{display:'none'}} */>
-                    <h1>Question {currentquestion+1}</h1>
+                    <h1>Question {currentquestion + 1}</h1>
                     <p>{Question[currentquestion].question}</p>
                     <div>
                         {
-                            Question[currentquestion].options.map((data,index)=>{
-                                    return  <button key={index}>{data}</button>
+                            Question[currentquestion].options.map((data, index) => {
+                                return <button key={index} onClick={() => { AddData(data) }}>{data}</button>
                             })
                         }
-                    
+
                     </div>
-                    <div className='time'>Time Left :<span>10S</span></div>
+                    <div className='time'>Time Left :<span>{timer}S</span></div>
                 </div>
             }
-
-
-
         </div>
     )
 }
